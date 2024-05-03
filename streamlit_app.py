95c0b47e16fe6f5d896a4545656955409110219e
vaibhavtgithub <vaibhavtagalpallewar@gmail.com>
2024-05-03 12:58:53 +0530

    Create streamlit_app.py
    copied from my Custom Smoothie SiS app

---
+++ streamlit_app.py
@@ -0,0 +1,59 @@
+# Import python packages
+import streamlit as st
+from snowflake.snowpark.context import get_active_session
+from snowflake.snowpark.functions import col
+
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie:cup_with_straw:")
+st.write(
+    """Replace this example with your own code!
+    **And if you're new to Streamlit,** check
+    out our easy-to-follow guides at
+    [docs.streamlit.io](https://docs.streamlit.io).
+    """
+)
+
+
+name_on_order = st.text_input('Name on Smoothie:')
+st.write('The Name on Your Smoothie will be', name_on_order)
+
+#option = st.selectbox(
+    #'How would you like to be contacted?',
+    #('Email', 'Home phone', 'Mobile phone'))
+
+#option = st.selectbox(
+    #'What is Your favorite Fruit?',
+    #('Banana', 'Strawberries', 'Peaches'))
+
+#st.write('Your favorite Fruit is:', option)
+
+
+
+session = get_active_session()
+my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
+#st.dataframe(data=my_dataframe, use_container_width=True)
+
+ingredients_list = st.multiselect('Choose upto 5 Ingredients:',my_dataframe,max_selections=5)
+
+if ingredients_list:
+    #st.write(ingredients_list)
+    #st.text(ingredients_list)
+
+    ingredients_string=''
+
+    for fruit_chosen in ingredients_list:
+        ingredients_string+=fruit_chosen + ' '
+
+    #st.write(ingredients_string)
+
+    my_insert_stmt = """ insert into smoothies.public.orders(ingredients,name_on_order)
+            values ('""" + ingredients_string + """','""" + name_on_order + """')"""
+
+    #st.write(my_insert_stmt)
+    #st.stop()
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        session.sql(my_insert_stmt).collect()
+        
+        st.success('Your Smoothie is ordered,' + name_on_order, icon="✅")
